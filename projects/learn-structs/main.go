981b188fe00fdf0593955a0164dc6299b86c5f65
Farid <fnikman@gmail.com>
2022-07-23 23:19:00 +0800

    feat: structs with pointer

--- projects/learn-structs/main.go
+++ projects/learn-structs/main.go
@@ -13,15 +13,26 @@ type person struct {
 	contactInfo
 }
 
+func (p person) greet() {
+	fmt.Printf("hello! my name is %v %v. Nice to meet you\n", p.firstName, p.lastName)
+}
+
+func (p *person) updateName(newName string) {
+	(*p).firstName = newName
+}
+
 func main() {
 	jim := person{
 		firstName: "Jim",
 		lastName:  "Parry",
-		contact: contactInfo{
+		contactInfo: contactInfo{
 			email:   "jim@email.com",
 			zipCode: 9400,
 		},
 	}
 
-	fmt.Printf("%v\n",jim)
+	jim.greet()
+	jimPointer := &jim
+	jimPointer.updateName("Jimmy")
+	jim.greet()
 }
