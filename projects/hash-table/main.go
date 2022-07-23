cbf69f5b5c4123eb57e877c8dfd30b781a183fe9
Farid <fnikman@gmail.com>
2022-07-23 15:35:36 +0800

    feat: insert in bucket

--- projects/hash-table/main.go
+++ projects/hash-table/main.go
@@ -19,10 +19,47 @@ type HashTable struct {
 	array [ARRAY_SIZE]*bucket
 }
 
+func hash(key string) int {
+	sum := 0
+
+	for _, v := range key {
+		sum += int(v)
+	}
+
+	return sum % ARRAY_SIZE
+}
+
+// insert will take in a key and add it to the hash table array
+func (h *HashTable) Insert(key string) {
+	index := hash(key)
+	h.array[index].insert(key)
+
+}
+
+// search will take in a key and return true if exist
+func (h *HashTable) Search(key string) {
+
+}
+
+// insert will take in a key and delete from the hash table
+func (h *HashTable) Delete(key string) {
+
+}
+
 // bucket structure
 type bucket struct {
 	// store address of bucketNode
 	head *bucketNode
+}
+
+func (b *bucket) insert(k string) {
+	newNode := &bucketNode{key: k}
+
+	// become the current head
+	newNode.next = b.head
+
+	// assign a new head
+	b.head = newNode
 }
 
 // bucketNode structure
@@ -43,4 +80,7 @@ func InitHashTable() *HashTable {
 func main() {
 	testHashTable := InitHashTable()
 	fmt.Println(testHashTable)
+
+  testBucket := &bucket{}
+  testBucket.insert("RANDY")
 }
