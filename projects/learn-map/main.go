da104c907a452697daf952596f3c4e18dacc8c8c
Farid <fnikman@gmail.com>
2022-08-07 00:01:11 +0800

    feat: manipulating map

--- projects/learn-map/main.go
+++ projects/learn-map/main.go
@@ -3,10 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#333",
-	}
+	colors := make(map[string]string)
+
+	//var colors map[string]string
+
+	//colors := map[string]string{
+	//	"red":   "#ff0000",
+	//	"green": "#333",
+	//}
+
+	// add values into map
+	colors["white"] = "#fff"
+	colors["red"] = "#ff0000"
+
+	// delete function
+	delete(colors, "white")
 
 	fmt.Println(colors)
 }
