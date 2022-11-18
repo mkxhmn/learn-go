dd61f7d41df67300c843f3bd1b0907e42aa45b0c
Farid <fnikman@gmail.com>
2022-11-18 23:22:55 +0800

    feat: this is still a buggy code, to showcase channel in go

--- projects/learn-channel-and-go-routine/main.go
+++ projects/learn-channel-and-go-routine/main.go
@@ -12,21 +12,27 @@ func main() {
 		"https://golang.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
 		// remember we only use 'go' keyword in front of function call.
 		// this is to invoke concurrency
-		go sequentialCheckLink(link)
+		go sequentialCheckLink(link, c)
 	}
+
+	fmt.Println(<-c)
 }
 
 // sequential approach
-func sequentialCheckLink(link string) {
+func sequentialCheckLink(link string, c chan string) {
 	_, err := http.Get(link)
 
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "might be down I think"
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- "yup it is up"
 }
