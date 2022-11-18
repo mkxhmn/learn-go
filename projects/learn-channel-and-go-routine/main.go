20eae652bc4330468ce034c70920f8141c25359e
Farid <fnikman@gmail.com>
2022-11-18 22:33:41 +0800

    feat: printing site status

--- projects/learn-channel-and-go-routine/main.go
+++ projects/learn-channel-and-go-routine/main.go
@@ -1,1 +1,29 @@
-package learn_channel_and_go_routine
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"https://google.com",
+		"https://stackoverflow.com",
+		"https://golang.com",
+	}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		return
+	}
+
+	fmt.Println(link, "is up!")
+}
