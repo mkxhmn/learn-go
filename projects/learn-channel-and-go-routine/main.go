45e5de9b11987ce79a13460ec02f1155746a3d9a
Farid <fnikman@gmail.com>
2022-11-19 23:35:34 +0800

    feat: sleeping routines, this is inappropiate placement to include sleep routines

--- projects/learn-channel-and-go-routine/main.go
+++ projects/learn-channel-and-go-routine/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -32,6 +33,7 @@ func main() {
 	// }
 
 	for l := range c {
+		time.Sleep(5 * time.Second)
 		go sequentialCheckLink(l, c)
 	}
 }
